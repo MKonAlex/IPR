291f17a2ccafafddea33b2fa67a22b058a70495e
MKon <m.kononovich@napopravku.ru>
2023-04-19 14:28:22 +0300

    init

---
+++ src/index.php
@@ -0,0 +1,25 @@
+<?php
+$sum = static fn(int $a, int $b): int => $a + $b;
+$join = static fn(string $a, string $b) => $a . $b;
+$calc = static function ($arg) use (&$calc) {
+    static $args;
+
+    if (is_callable($arg)) {
+        $result = is_string($args[0])
+            ? ''
+            : 0;
+
+
+        foreach ($args as $a) {
+            $result = $arg($result ?? 0, $a);
+        }
+        $args = null;
+        return $result;
+    } else {
+        $args[] = $arg;
+        return $calc;
+    }
+};
+echo $calc(1)(2)($sum); // 3
+echo $calc(1)(2)(7)($sum); // 10
+echo $calc('a')('b')('c')('d')($join);
